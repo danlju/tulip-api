3efd0205dc9db6f5615d931e355863222f5ffcee
danlju <danlju@proton.me>
2025-11-08 09:42:27 +0900

    sort builds by number

--- src/main/java/com/danlju/tulip/infrastructure/db/BuildCrudRepository.java
+++ src/main/java/com/danlju/tulip/infrastructure/db/BuildCrudRepository.java
@@ -1,6 +1,7 @@
 package com.danlju.tulip.infrastructure.db;
 
 import com.danlju.tulip.infrastructure.db.entity.BuildDbEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.UUID;
 public interface BuildCrudRepository extends CrudRepository<BuildDbEntity, Integer> {
     BuildDbEntity findByPublicId(UUID id);
     BuildDbEntity findByExternalId(String externalId);
+    @Query("from BuildDbEntity b where b.project.id=?1 order by b.number desc")
     List<BuildDbEntity> findByProjectId(Integer id);
 }
